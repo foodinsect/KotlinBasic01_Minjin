c68d908fbca238344578c0d4808fd96c772fbc2b
Minjin <opqrs0422@naver.com>
2023-06-26 15:24:57 +0900

    토스트 띄워보기

--- app/src/main/java/com/neppplus/kotlinbasic01_minjin/MainActivity.kt
+++ app/src/main/java/com/neppplus/kotlinbasic01_minjin/MainActivity.kt
@@ -3,10 +3,15 @@ package com.neppplus.kotlinbasic01_minjin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.neppplus.kotlinbasic01_minjin.databinding.ActivityMainBinding // 추가
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding // 추가
+// 코드에 메모 남기기 - 주석
+    /*
+     Ctrl + / : 커서가 있는 줄을 주석 처리
+     이 {  } 안에 있는 코드만, 버튼이 눌렸을 때 실행됨  */
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +23,9 @@ class MainActivity : AppCompatActivity() {
             Log.e("메인화면로그","에러 관련 로그")
         }
 
+        binding.smallBtn.setOnClickListener {
+            Toast.makeText(this, "작은 버튼 눌림", Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
